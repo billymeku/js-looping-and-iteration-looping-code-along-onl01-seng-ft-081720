b12183a1ba76c1aa87b619fb601d612120047af2
blen_meku <mekuriablen@gmail.com>
2020-11-19 23:13:56 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const messages = [];
+function writeCards(ppl, event) {
+    for (let i = 0; i < ppl.length; i++) {
+        messages.push(`Thank you, ${ppl[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages;
+}
+
+
+function countDown(n){
+while (n >= 0) {
+    console.log(n--);
+  }
+}
